f2cce2472dac34df1aad2a2f455e965caaa26715
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-03-30 13:07:12 +0300

    Generate a country name

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,34 @@
-var greeter = (name) => `Hello, ${name}!`;
+(function () {
+  let component,
+      countryName,
+      imageSrc;
 
-console.log(greeter('User'));
+  let getTemplate = () => `
+    <h2>${countryName}</h2>
+    <div>
+      <img class="country-image"
+           src="${imageSrc}">
+    </div>
+  `;
+
+  function updateModel () {
+    return new Promise(resolve => {
+      countryName = new window.Chance().country({full: true});
+      resolve();
+    });
+  }
+
+  function updateComponent () {
+    component.style.opacity = 0;
+    updateModel().then(() => {
+      component.innerHTML = getTemplate();
+      component.style.opacity = 1;
+    });
+  }
+
+  window.addEventListener('load', function () {
+    component = document.getElementById('js-component-container');
+    updateComponent();
+    document.getElementById('js-update-button').addEventListener('click', updateComponent);
+  });
+})();
